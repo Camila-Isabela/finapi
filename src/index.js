9c4b30b21c76a9cfc2130f1821ffe8ce8bc9e407
Camila Isabela <53484905+Camila-Isabela@users.noreply.github.com>
2022-11-14 12:29:48 -0300

    middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
+const customers = []
 
 /**
  * cpf       - string
@@ -9,8 +11,18 @@ const app = express()
  * statement - []
  */
 
+app.use(express.json())
+
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body
+    const id = uuidv4()
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    })
+    return response.status(201).send()
 })
 
 
