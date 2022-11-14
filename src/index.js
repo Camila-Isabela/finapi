5608dbda1f374d63405a6e858b3bfff4ce2c3f3e
Camila Isabela <53484905+Camila-Isabela@users.noreply.github.com>
2022-11-14 12:08:44 -0300

    uuid

--- src/index.js
+++ src/index.js
@@ -2,4 +2,19 @@ const express = require('express')
 
 const app = express()
 
-app.get()
+/**
+ * cpf       - string
+ * name      - string
+ * id        - uuid
+ * statement - []
+ */
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body
+})
+
+
+
+
+
+app.listen(3333)
