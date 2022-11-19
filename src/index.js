bd5a6ccb59e7ec27b1fac3ab72c63b8afd2d4690
Camila-Isabela <camila_isabela_lins@hotmail.com>
2022-11-19 12:00:15 -0300

    deposit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+const { response } = require('express')
 const express = require('express')
+const { request } = require('http')
+const { type } = require('os')
 
 const { v4: uuidv4 } = require('uuid')
 
@@ -47,6 +50,29 @@ app.get("/statement", verifyExistsAccountCPF, (request, response) => {
     const { customer } = request
 
     return response.json(customer.statement)
+})
+
+app.post("/deposit", verifyExistsAccountCPF, (request, response) => {
+
+    const { description, amount } = request.body
+
+    const { customer } = request
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation)
+
+    return response.status(201).send()
+
+})
+
+app.post("/withdraw", (request, response) => {
+    
 })
 
 app.listen(3333)
